a566c13f1ec5b9fe2e742fa71abcaa72b624ea7b
uh_male <kkinzler@pdx.edu>
2020-08-03 17:05:38 -0700

    new

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+const config = {
+    apiKey: "AIzaSyBJiUujB8aiGHXm8jxBAChHUWsnJqJERgM",
+    authDomain: "capstone-spsu2020.firebaseapp.com",
+    databaseURL: "https://capstone-spsu2020.firebaseio.com",
+    projectId: "capstone-spsu2020",
+    storageBucket: "capstone-spsu2020.appspot.com",
+    messagingSenderId: "834378217953",
+    appId: "1:834378217953:web:329c8b8dbbef0959d23aa0",
+    measurementId: "G-V33QNYQNZL"
+};
+
+export default config;
